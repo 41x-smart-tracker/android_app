933007d73794a1ca2892e5834321b6f28ffe01d4
alberttir <albert710@gmail.com>
2015-03-31 14:25:33 -0400

    NodeStore updated

--- app/src/main/java/wifismarttracker/smarttracker/NodeStore.java
+++ app/src/main/java/wifismarttracker/smarttracker/NodeStore.java
@@ -3,6 +3,17 @@ package wifismarttracker.smarttracker;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.net.wifi.WifiManager;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import java.util.ArrayList;
 
@@ -28,20 +39,56 @@ public class NodeStore {
         _nodes.add(new Node("2468", "roam", "eduroam", _wifiManager, _sensorManager));
     }
 
-    public void saveNode(Node toSave)
+    public void saveNode(Node toSave, Context ctx)
     {
+
+        //String filename = "smartTrackerRegistry.txt";
+        String string;
+        FileOutputStream outputStream;
         // open file
         // write node in text format
         // openFileOutput(nodeFileName, Context.MODE_PRIVATE)
         // close file
+
+        string = toSave.ssid() + ';' + toSave.name() + ';' + toSave.angle() + ';' + toSave.distance() + ';';
+
+        try {
+            outputStream = ctx.openFileOutput(nodeFileName, Context.MODE_APPEND | Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void deleteNode(Node toDelete)
+    public void deleteNode(Node toDelete, Context ctx) throws IOException
     {
+        String currentLine;
+        String toDeleteString;
+
         // open file
         // seek and destroy
         // openFileInput(nodeFileName, Context.MODE_PRIVATE)
         // close file
+
+        File inputFile = new File(nodeFileName);
+        File tempFile = new File("nodeFileName_temp.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        toDeleteString = toDelete.ssid() + ';' + toDelete.name() + ';' + toDelete.angle() + ';' + toDelete.distance() + ';';
+
+        // checks line by line if it's the string we want, if it matches, skip that line (delete), otherwise copy line to temp file
+        while((currentLine = reader.readLine()) != null) {
+            if(null!=currentLine && !currentLine.equalsIgnoreCase(toDeleteString)){
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+        }
+        writer.close();
+        reader.close();
+        boolean successful = tempFile.renameTo(inputFile); // renames the temp file to the original file
+
     }
 
     public ArrayList<Node> getAllNodes()
